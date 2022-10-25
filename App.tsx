b0d5eeb95efd25c852c3a975aac78418e9ba935f
ajonescode <54915239+ajonescode@users.noreply.github.com>
2022-10-25 15:23:59 -0500

    2 files modified, 1 added

--- App.tsx
+++ App.tsx
@@ -28,6 +28,11 @@ export default function App() {
       <source src="https://anthonyjonesmusic.s3.us-east-2.amazonaws.com/Bounced+Trax/24hrs.wav"></source>
     </audio>
     <figcaption>
+    <img
+        class="small-pic"
+        src=""
+        alt=""
+      />
       <span class="name-box">41 Lake Park</span>
     </figcaption>
     <audio controls>
@@ -35,6 +40,11 @@ export default function App() {
     </audio>
 
     <figcaption>
+    <img
+        class="small-pic"
+        src=""
+        alt=""
+      />
       <span class="name-box">69 Butterflies</span>
     </figcaption>
     <audio controls>
@@ -42,16 +52,17 @@ export default function App() {
     </audio>
 
     <figcaption>
+    <img
+        class="small-pic"
+        src=""
+        alt=""
+      />
       <span class="name-box">Bandino Mayhem 3</span>
     </figcaption>
     <audio controls>
       <source src="https://anthonyjonesmusic.s3.us-east-2.amazonaws.com/Bounced+Trax/bandinomayhem3.wav"></source>
     </audio>
-
-    <figcaption></figcaption>
-    <audio controls>
-      <source src=""></source>
-    </audio>
+    
     <figcaption>Fly prod. By Bandino</figcaption>
     <audio controls>
       <source src="https://anthonyjonesmusic.s3.us-east-2.amazonaws.com/Bounced+Trax/fly+prod+bandino+88_Master.wav"></source>
