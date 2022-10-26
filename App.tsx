4ee04aabfbda2ea8e3b5a9a270c3bef5bc1dc69b
ajonescode <54915239+ajonescode@users.noreply.github.com>
2022-10-26 11:40:42 -0500

    All tracks converted to Trackview Component

--- App.tsx
+++ App.tsx
@@ -10,51 +10,17 @@ export default function App() {
     <div>
   
     <Navbar />
-    <Trackview Trackname = "24 hrs" AudioTrack = "https://anthonyjonesmusic.s3.us-east-2.amazonaws.com/Bounced+Trax/24hrs.wav"/>
+ 
     
     <section class="Music-Block">
+    <Trackview Trackname = "24 hrs" AudioTrack = "https://anthonyjonesmusic.s3.us-east-2.amazonaws.com/Bounced+Trax/24hrs.wav" Picture = "https://anthonyjonesphotolife.s3.us-east-2.amazonaws.com/Photos/pnghut_spaceflight-illustration-wall-clock-aerospace-science-and-technology-space-distant.png"/>
+    <Trackview Trackname = "41 Lake Park" AudioTrack = "https://anthonyjonesmusic.s3.us-east-2.amazonaws.com/Bounced+Trax/41lakepark.mp3"/>
+    <Trackview Trackname = "69 Butterflies" AudioTrack = "https://anthonyjonesmusic.s3.us-east-2.amazonaws.com/Bounced+Trax/69+butterflies+187+%5BNo+808%5D_Master.wav"/>
+    <Trackview Trackname = "Bandino Mayhem 3" AudioTrack = "https://anthonyjonesmusic.s3.us-east-2.amazonaws.com/Bounced+Trax/bandinomayhem3.wav"/>
+    <Trackview Trackname = "Fly prod. By Bandino" AudioTrack = "https://anthonyjonesmusic.s3.us-east-2.amazonaws.com/Bounced+Trax/fly+prod+bandino+88_Master.wav" />
     
-    
-    <figcaption>
-    <img
-        class="small-pic"
-        src=""
-        alt=""
-      />
-      <span class="name-box">41 Lake Park</span>
-    </figcaption>
-    <audio controls>
-      <source src="https://anthonyjonesmusic.s3.us-east-2.amazonaws.com/Bounced+Trax/41lakepark.mp3"></source>
-    </audio>
 
-    <figcaption>
-    <img
-        class="small-pic"
-        src=""
-        alt=""
-      />
-      <span class="name-box">69 Butterflies</span>
-    </figcaption>
-    <audio controls>
-      <source src="https://anthonyjonesmusic.s3.us-east-2.amazonaws.com/Bounced+Trax/69+butterflies+187+%5BNo+808%5D_Master.wav"></source>
-    </audio>
 
-    <figcaption>
-    <img
-        class="small-pic"
-        src=""
-        alt=""
-      />
-      <span class="name-box">Bandino Mayhem 3</span>
-    </figcaption>
-    <audio controls>
-      <source src="https://anthonyjonesmusic.s3.us-east-2.amazonaws.com/Bounced+Trax/bandinomayhem3.wav"></source>
-    </audio>
-    
-    <figcaption>Fly prod. By Bandino</figcaption>
-    <audio controls>
-      <source src="https://anthonyjonesmusic.s3.us-east-2.amazonaws.com/Bounced+Trax/fly+prod+bandino+88_Master.wav"></source>
-    </audio>
   </section>
     </div>
   );
